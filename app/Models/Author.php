b82e7200f977776bcff0d2485ea0da14f0bb803e
azlinliana <azlinliana.adlan@gmail.com>
2022-09-02 01:03:32 +0800

    Migrate authors table

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     use HasFactory;
+
+    // Protected fillable can ensure that we can use create method on eloquent request 
+    // $fillable attribute is an array containing fields of table which can be filled using mass-assignment
+    // Mass-assignment is a process of sending an array of data that will be saved to the specified model at once. In general, you don’t need to save data on your model on one by one basis, but rather in a single process.
+    protected $fillable = [
+        'name'
+    ];
 }
