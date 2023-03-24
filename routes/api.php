2f564a39c7e0c45cfe3fb2ef815d7e8f8058943b
azlinliana <azlinliana.adlan@gmail.com>
2023-03-24 14:36:41 +0800

    CRUD and search products

--- routes/api.php
+++ routes/api.php
@@ -40,5 +40,10 @@ Route::middleware('auth:api')->prefix('v1')->group(function() {
     Route::apiResource('/books', BooksController::class);
 });
 
-// Simple Products CRUD
-
+// Simple Products CRUD and search
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
